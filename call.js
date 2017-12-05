7efb8b17caabe514bb12c7a449060c8f3aca2ef2
DeronLee <akagilnc@gmail.com>
2017-12-05 23:13:50 +0800

    add async

--- call.js
+++ call.js
@@ -1,9 +1,13 @@
-var request = require('request');
+const request = require('request');
+async function call () {
 
-begin_time = Date.now()
-console.log(begin_time)
-request.post('https://git-dev.leanapp.cn/activity/')
-end_time = Date.now()
-console.log(end_time)
+    let begin_time = Date.now()
+    console.log(begin_time)
+    await request.post('https://git-dev.leanapp.cn/activity/')
+    let end_time = Date.now()
+    console.log(end_time)
 
-console.log('used ' + (end_time - begin_time) + 'ms')
+    console.log('used ' + (end_time - begin_time) + 'ms')
+}
+
+call()
