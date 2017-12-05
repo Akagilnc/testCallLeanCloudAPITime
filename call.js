006ec3724c977c47042adff19f559fd3f8c0b8a6
DeronLee <akagilnc@gmail.com>
2017-12-05 23:29:06 +0800

    fixed

--- call.js
+++ call.js
@@ -1,13 +1,13 @@
 const request = require('request');
 async function call () {
 
-    let begin_time = Date.now()
-    console.log(begin_time)
-    await request.post('https://git-dev.leanapp.cn/activity/')
-    let end_time = Date.now()
-    console.log(end_time)
+    var fetch = require("node-fetch");
 
-    console.log('used ' + (end_time - begin_time) + 'ms')
+    console.time('test');
+
+    await fetch('https://git-dev.leanapp.cn/activity/');
+
+    console.timeEnd('test');
 }
 
 call().then((v)=>{})
